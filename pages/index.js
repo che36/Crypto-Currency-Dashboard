c5a5b7a795b9b81d0766ebb1c0796046de809bb1
che36 <tron36.cc@gmail.com>
2021-01-07 23:07:42 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,65 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import CoinGecko from 'coingecko-api';
+const coinGeckoClient = new CoinGecko();
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Home(props) {
+	const { data } = props.result;
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+	const formatPercent = (number) => `${new Number(number).toFixed(2)}%`;
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+	const formatDollar = (number, maximumSignificantDigits) => {
+		new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'usd',
+			maximumSignificantDigits
+		}).format(number);
+	};
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Coinmarketcap Clone</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1>Coinmarketcap Clone</h1>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Symbol</th>
+						<br />
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((coin) => (
+						<tr key={coin.id}>
+							<td>
+								<img src={coin.image} style={{ width: 25, height: 25, marginRight: 10 }} />
+							</td>
+							<td>{coin.symbol.toUppercase}</td>
+							<td>
+								<span className={coin.price_change_percentage_24h > 0 ? 'text-sucess' : 'text-danger'}>
+									{formatPercent(coin.price_change_percentage_24h)}
+								</span>
+							</td>
+							<td>{formatDollar(coin.current_price, 20)}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+}
+export async function getServerSideProps(context) {
+	const params = {
+		order: CoinGecko.ORDER.MARKET_CAP_DESC
+	};
+	const result = await coinGeckoClient.coins.markets({ params });
+	return {
+		props: {
+			result
+		}
+	};
 }
